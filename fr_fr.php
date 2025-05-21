0e5cc5413389d3a9cb03e84fd5e55352163e8c85
RoxzyWeb <stussyopera@gmail.com>
2025-05-21 12:07:40 +0200

    Create fr_fr.php

---
+++ fr_fr.php
@@ -0,0 +1,15 @@
+<?php
+// fr_fr.php - page simple en français
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <title>Page Française</title>
+</head>
+<body>
+    <h1>Salut !</h1>
+    <p>Bienvenue sur mon site multilingue.</p>
+</body>
+</html>
